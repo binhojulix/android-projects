2ff9e2cf23fdee497e6c82eb551aee49053ea907
binhojulix <fabio.jdluz@gmail.com>
2015-12-24 11:14:01 -0200

    projetos

---
+++ DemoComponenteAndroid/app/src/main/java/com/example/aureliosy/democomponenteandroid/models/Contato.java
@@ -0,0 +1,27 @@
+package com.example.aureliosy.democomponenteandroid.models;
+
+/**
+ * Created by aureliosy on 01/12/2015.
+ */
+public class Contato {
+
+    private String nome;
+    private String telefone;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+
+}
+
